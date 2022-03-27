fd29f7d0c5f5518efe35464d79fbdcb7f8345388
web3ten0 <web3ten0@gmail.com>
2022-03-28 00:04:59 +0900

    added TestUserGetById

--- delivery/http/handler/user_test.go
+++ delivery/http/handler/user_test.go
@@ -16,21 +16,35 @@ func TestUserGetAll(t *testing.T) {
 	var mockUser domain.User
 	err := faker.FakeData(&mockUser)
 	assert.NoError(t, err)
-	req, err := http.NewRequest("GET", "/users", nil)
-	assert.NoError(t, err)
-	rec := httptest.NewRecorder()
-	ginContext, _ := gin.CreateTestContext(rec)
-	ginContext.Request = req
-	u := new(usecase.MockUserUsecase)
 	mockUserList := []*domain.User{&mockUser}
+	u := new(usecase.MockUserUsecase)
 	u.On("GetAll").Return(mockUserList, nil)
 	h := NewUserHandler(u)
-	h.GetAll(ginContext)
-	assert.Equal(t, 200, rec.Code)
+	w := httptest.NewRecorder()
+	_, r := gin.CreateTestContext(w)
+	r.GET("/users", h.GetAll)
+	req, err := http.NewRequest("GET", "/users", nil)
+	assert.NoError(t, err)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
 }
 
 func TestUserGetById(t *testing.T) {
-	if false {
-		t.Error("oh my god")
+	var mockUser domain.User
+	err := faker.FakeData(&mockUser)
+	assert.NoError(t, err)
+	u := new(usecase.MockUserUsecase)
+	u.On("GetById").Return(&mockUser, nil)
+	h := NewUserHandler(u)
+
+	cases := map[string]int{"1": 200, "a": 400}
+	for id, code := range cases {
+		w := httptest.NewRecorder()
+		_, r := gin.CreateTestContext(w)
+		r.GET("/users/view/:id", h.GetById)
+		req, err := http.NewRequest("GET", "/users/view/"+id, nil)
+		assert.NoError(t, err)
+		r.ServeHTTP(w, req)
+		assert.Equal(t, code, w.Code)
 	}
 }
